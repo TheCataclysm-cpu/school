6d199df4d8ab81a49e73446dddf5db2c9bf512a3
TheCataclysm-cpu <mihai.belet12@gmail.com>
2020-10-12 01:05:13 +0300

    Lab1 Complet

--- invatare automata/main.py
+++ invatare automata/main.py
@@ -4,8 +4,15 @@ import random
 import math
 
 def gauss_convert(value,m_value,sigma_value):
-    gauss_value = math.exp(-(pow(m_value-value, 2))/(2 * pow(sigma_value)))
+    gauss_value = math.exp(-(pow(m_value-value, 2))/(2 * pow(sigma_value,2)))
     return gauss_value
+
+def choose_coordonate_random(sigma,m):
+    prag = random.uniform(0,1)
+    random_coordonate = random.randint(-300,300)
+    while gauss_convert(random_coordonate,m,sigma) <= prag:
+        random_coordonate = random.randint(-300, 300)
+    return random_coordonate
 
 def main():
     graph_size = config.interval
@@ -13,10 +20,13 @@ def main():
     zones = config.zones
     plt.xlim(graph_size[0],graph_size[1])
     plt.ylim(graph_size[0],graph_size[1])
-    plt.show()
     for i in range(0,10000):
         random_zone_key = random.choice(list(zones))
-        random_x = random.randint(interval[0],interval[1])
+        rand_x = choose_coordonate_random(zones[random_zone_key]['interval']['sigmax'],zones[random_zone_key]['interval']['mx'])
+        rand_y = choose_coordonate_random(zones[random_zone_key]['interval']['sigmay'],zones[random_zone_key]['interval']['my'])
+        plt.scatter(rand_x,rand_y,c=zones[random_zone_key]['color'],s=1)
+        print("stage " + str(i))
+    plt.show()
 
 
 if __name__ == "__main__":
--- invatare automata/resources/config.py
+++ invatare automata/resources/config.py
@@ -4,24 +4,33 @@ zones = {
         'interval' : {
             'x' : [0,300],
             'y' : [0,300],
-            'mx' : 40,
-            'my' : 60
-        }
+            'mx' : 180,
+            'my' : 220,
+            'sigmax' : 10,
+            'sigmay' : 10
+        },
+        'color' : 'red'
     },
     'zone2' : {
         'interval' : {
             'x' : [-300,0],
             'y' : [0,300],
-            'mx' : -40,
-            'my' : 60
-        }
+            'mx' : -100,
+            'my' : 110,
+            'sigmax' : 15,
+            'sigmay' : 10
+        },
+        'color' : 'blue'
     },
     'zone3' : {
         'interval' : {
             'x' : [0,300],
             'y' : [-300,0],
-            'mx' : 40,
-            'my' : -60
-        }
+            'mx' : 210,
+            'my' : -150,
+            'sigmax' : 5,
+            'sigmay' : 20
+        },
+        'color' : 'yellow'
     }
 }

7c11cee5b15968d4084cf4afb9cf16228159f6ef
TheCataclysm-cpu <mihai.belet12@gmail.com>
2020-11-01 22:03:41 +0200

    final unu inceput doi

--- invatare automata/lucrarea 1/main.py
+++ invatare automata/lucrarea 1/main.py
@@ -2,15 +2,17 @@ from resources import config
 import matplotlib.pyplot as plt
 import random
 import math
+import numpy as np
 
 def gauss_convert(value,m_value,sigma_value):
     gauss_value = math.exp(-(pow(m_value-value, 2))/(2 * pow(sigma_value,2)))
     return gauss_value
 
 def choose_coordonate_random(sigma,m):
-    prag = random.uniform(0,1)
+    prag = random.randint(0,100)
+    prag = prag / 100
     random_coordonate = random.randint(-300,300)
-    while gauss_convert(random_coordonate,m,sigma) <= prag:
+    while gauss_convert(random_coordonate,m,sigma) < prag:
         random_coordonate = random.randint(-300, 300)
     return random_coordonate
 
@@ -24,8 +26,8 @@ def main():
         random_zone_key = random.choice(list(zones))
         rand_x = choose_coordonate_random(zones[random_zone_key]['interval']['sigmax'],zones[random_zone_key]['interval']['mx'])
         rand_y = choose_coordonate_random(zones[random_zone_key]['interval']['sigmay'],zones[random_zone_key]['interval']['my'])
+
         plt.scatter(rand_x,rand_y,c=zones[random_zone_key]['color'],s=1)
-        print("stage " + str(i))
     plt.show()
 
 
